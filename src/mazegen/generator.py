82317f42f2d563b195a2f3d4795f73c92b876c68
Luciano Petillo <lupetill@c4b7c1.42berlin.de>
2026-08-08 22:47:30 +0200

    generator.py - cells vinculation. recursion backtracking (iterative stack)

--- src/mazegen/generator.py
+++ src/mazegen/generator.py
@@ -1,20 +1,25 @@
 from typing import Optional
-
+import random
 
 class Cell:
     def __init__(self) -> None:
+        self.n: Optional[Cell] = None
+        self.e: Optional[Cell] = None
+        self.s: Optional[Cell] = None
+        self.w: Optional[Cell] = None
         self.walls = 0b1111
         self.visited = False
 
 
 class MazeGenerator:
-    def __init__(self, width: int, height: int, seed: Optional[int] = None) -> None:
+    def __init__(self, width: int, height: int,
+                 seed: Optional[int] = None) -> None:
         self.width = width
         self.height = height
         self.seed = seed
         self.grid: list[list[Cell]] = []
 
-    def create_grid(self) -> None:
+    def grid_init(self) -> None:
         for _ in range(self.height):
             row: list[Cell] = []
 
@@ -22,6 +27,48 @@ class MazeGenerator:
                 row.append(Cell())
 
             self.grid.append(row)
+
+        self._set_up_cells()
+        self._hammer()
+
+    def _set_up_cells(self) -> None:
+        for row in range(self.height):
+            for col in range(self.width):
+                if row > 0:
+                    self.grid[row][col].n = self.grid[row - 1][col]
+                if col < self.width - 1:
+                    self.grid[row][col].e = self.grid[row][col + 1]
+                if row < self.height - 1:
+                    self.grid[row][col].s = self.grid[row + 1][col]
+                if col > 0:
+                    self.grid[row][col].w = self.grid[row][col - 1]
+
+    def _backtracker(self) -> None:
+        curren = self.grid[0][0]
+        stack: list[Cell] = []
+        neighbors: list[Cell] = []
+
+        current.visited = True
+        stack.append(current)
+
+        while(stack)
+            neighbors = self._get_valid_neighbors(stack[-1])
+            next_cell = random.choice(neighbors)
+
+    def _get_valid_neighbors(self, cell: Cell) -> list[Cell]:
+
+        neighbors: list[Cell] = []
+
+        if cell.n is not None and not cell.n.visited:
+            neighbors.append(cell.n)
+        if cell.e is not None and not cell.e.visited:
+            neighbors.append(cell.e)
+        if cell.s is not None and not cell.s.visited:
+            neighbors.append(cell.s)
+        if cell.w is not None and not cell.w.visited:
+            neighbors.append(cell.w)
+
+        return neighbors
 
     def export(self, filename: str) -> None:
         with open(filename, "w") as file:
